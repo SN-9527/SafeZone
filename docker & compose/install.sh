84219745b7755dbba4b6d4d3c9c96ba6aa010209
SN-9527 <github@nireus.cc>
2025-06-26 22:15:55 +0800

    Update install.sh

--- docker & compose/install.sh
+++ docker & compose/install.sh
@@ -3,5 +3,5 @@ curl -fsSL https://get.docker.com | bash -s docker
 systemctl start docker && systemctl enable docker
 
 # 安装Docker-Compose
-curl -L "https://github.com/docker/compose/releases/download/v2.35.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.37.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
