4e6e4aef1b9af69b9ffdac22931d603675ce030f
SN-9527 <github@nireus.cc>
2026-01-29 15:32:47 +0800

    update

--- docker & compose/install.sh
+++
@@ -1,7 +0,0 @@
-# 安装Docker
-curl -fsSL https://get.docker.com | bash -s docker
-systemctl start docker && systemctl enable docker
-
-# 安装Docker-Compose
-curl -L "https://github.com/docker/compose/releases/download/v2.37.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
---
+++ 常用命令/linux.sh
@@ -0,0 +1,10 @@
+# 安装Docker
+curl -fsSL https://get.docker.com | bash -s docker
+systemctl start docker && systemctl enable docker
+
+# 安装Docker-Compose
+curl -L "https://github.com/docker/compose/releases/download/v2.40.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#安装xray
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u root
